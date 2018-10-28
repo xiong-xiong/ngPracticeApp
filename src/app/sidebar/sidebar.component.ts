0ea847625f113446449b209f94abdc3c561cd7fd
Juha Stenroos <juha.stenroos@edita.fi>
2018-10-28 23:32:07 +0200

    add animations package

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"]
 })
 export class SidebarComponent implements OnInit {
+  currentUrl: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    router.events.subscribe((_: NavigationEnd) => (this.currentUrl = _.url));
   }
 
+  ngOnInit() {}
 }
