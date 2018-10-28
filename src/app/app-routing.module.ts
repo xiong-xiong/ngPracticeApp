ee07b6962a54f53c2e21e1d3357f6548aa04d193
Juha Stenroos <juha.stenroos@edita.fi>
2018-10-28 23:07:49 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { UsersComponent } from "./users/users.component";
+import { DetailsComponent } from "./details/details.component";
+import { PostsComponent } from "./posts/posts.component";
+import { PostComponent } from "./post/post.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: UsersComponent
+  },
+  {
+    path: "details/:id",
+    component: DetailsComponent
+  },
+  {
+    path: "posts",
+    component: PostsComponent
+  },
+  {
+    path: "postdetails/:id",
+    component: PostComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { PostsComponent } from "./posts/posts.component";
+import { UsersComponent } from "./users/users.component";
+import { DetailsComponent } from "./details/details.component";
+
+import { HttpClientModule } from "@angular/common/http";
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PostsComponent,
+    UsersComponent,
+    DetailsComponent,
+    PostComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
